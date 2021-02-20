714277f5bd92b7d2c2576eb42192e8b812e90075
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-20 18:55:05 +0900

    詳細ページ内容追加

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -10,6 +10,41 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text('ID'),
+                  width: 50,
+                ),
+                Container(
+                  child: Text(' : '),
+                  width: 20,
+                ),
+                Text('毎回同じIDを表示'),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(10),),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text('PW'),
+                  width: 50,
+                ),
+                Container(
+                  child: Text(' : '),
+                  width: 20,
+                ),
+                Text('毎回同じパスワードを表示'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
